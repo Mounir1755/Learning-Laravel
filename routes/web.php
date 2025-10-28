6caa10ece6ad403b464a14c74b3ea030b8b791bc
Mounir1755 <mounirbalikane@outlook.com>
2025-10-28 14:02:08 +0100

    video 16

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,28 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => Job::with('employer')->paginate(3)
+    return view('jobs.index', [
+        'jobs' => Job::with('employer')->latest()->simplepaginate(3)
     ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+    Job::create([
+        'title' => request('title'),
+        'salary'=> request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
