ad070398c63826751f31f0d69cbedfbc6815c92a
Mounir1755 <mounirbalikane@outlook.com>
2025-11-03 09:55:48 +0100

    video 18

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,9 +3,11 @@
        Job listings
     <x-slot:heading>
     
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
 
     <p>
-        this job pays {{ $job['salary'] }} per year.
+        this job pays {{ $job->salary }} per year.
     </p>
+
+    <a href="{{ $job->id }}/edit">edit job</a>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -9,27 +9,57 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
+// Index
 Route::get('/jobs', function () {
     return view('jobs.index', [
         'jobs' => Job::with('employer')->latest()->simplepaginate(3)
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
     Job::create([
         'title' => request('title'),
         'salary'=> request('salary'),
         'employer_id' => 1
     ]);
+
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    $job = Job::findorfail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    return redirect('jobs/'. $job->id);
+});
+
+// Delete
+Route::delete('/jobs/{id}', function ($id) {
+    Job::findorfail($id)->delete();
 
     return redirect('/jobs');
 });
